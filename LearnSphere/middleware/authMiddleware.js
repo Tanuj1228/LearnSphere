66e14c3892b9822b4b11ef45c443e9d9288e1bc3
Jashanpreet Kaur Aujla <156998111+ijashanx@users.noreply.github.com>
2025-09-24 13:57:45 +0530

    Update authMiddleware.js

--- LearnSphere/middleware/authMiddleware.js
+++ LearnSphere/middleware/authMiddleware.js
@@ -35,7 +35,6 @@ exports.protect = async (req, res, next) => {
     }
 };
 
-// Middleware to check if the user is an admin
 exports.isAdmin = (req, res, next) => {
     if (req.user && req.user.role === 'admin') {
         next(); // User is an admin, proceed
